ea9bf4f6260bc9905284a0d3b274626e2bb1a735
Pyrohova <pyrohovas@ukr.net>
2020-09-20 00:26:06 +0300

    scene update

--- Assets/TestFolder/Try.cs
+++ Assets/TestFolder/Try.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(RectTransform))]
 public class Try : MonoBehaviour
 {
-    [SerializeField] GameObject chip;
-    // Start is called before the first frame update
+    private const int boardSize = 8;
+    [SerializeField] GameObject cell;
+    [SerializeField] GameObject blackChip;
+    [SerializeField] GameObject whiteChip;
+    [SerializeField] GameObject board;
+    public GameObject[,] boardCells;
+
     void Start()
     {
-        
+        boardCells = new GameObject[boardSize, boardSize];
+        GenerateBoard();
     }
 
     // Update is called once per frame
@@ -17,8 +25,30 @@ public class Try : MonoBehaviour
         
     }
 
+
+    void GenerateBoard()
+    {
+        float startX = -473, startY  = 530, step = 135;
+        for (int i = 0; i< boardSize; i++)
+        {
+            for (int j = 0; j < boardSize; j++)
+            {
+                boardCells[i, j] = Instantiate(cell, new Vector2(startX, startY), cell.transform.rotation);
+                boardCells[i, j].name = "cell" + i + j;
+                boardCells[i, j].transform.SetParent(board.transform);
+                startX += step;
+                if (j == boardSize - 1)
+                    startX = -473;
+            }
+
+            startY -= step;
+        }
+    }
+
     private void OnMouseDown()
     {
-        Instantiate(chip,transform.position, chip.transform.rotation);
+        Debug.Log("kek");
+        Instantiate(whiteChip, cell.transform.position, whiteChip.transform.rotation);
     }
+
 }
