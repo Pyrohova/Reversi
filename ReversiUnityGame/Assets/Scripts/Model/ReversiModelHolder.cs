bc7dfacaaeaa64cd60291d34b435444e96cca4e0
lenskaya <lenskaya.ekaterina@gmail.com>
2020-09-24 22:42:40 +0300

    Small fixes

--- ReversiUnityGame/Assets/Scripts/Model/ReversiModelHolder.cs
+++ ReversiUnityGame/Assets/Scripts/Model/ReversiModelHolder.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ReversiCore;
-using ReversiCore.Interfaces;
+﻿using ReversiCore;
 using UnityEngine;
-using Unity;
 
 namespace Assets.Scripts.Model
 {
     public class ReversiModelHolder : MonoBehaviour
     {
-        //IReversiModel reversiModel = ContainerHolder.Source.Container.Resolve<IReversiModel>();
         public ReversiModel reversiModel { get; private set; }  = new ReversiModel();
     }
 }
