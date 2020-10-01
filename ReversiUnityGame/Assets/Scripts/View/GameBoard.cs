cf78d2f45b005113e1c93e96264c21dd78ef4fc6
lenskaya <lenskaya.ekaterina@gmail.com>
2020-10-01 15:25:10 +0300

    Reversi finished

--- ReversiUnityGame/Assets/Scripts/View/GameBoard.cs
+++ ReversiUnityGame/Assets/Scripts/View/GameBoard.cs
@@ -1,7 +1,7 @@
 ﻿using ReversiCore;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 using ChipColor = ReversiCore.Enums.Color;
 
 namespace Assets.Scripts.View
@@ -11,17 +11,36 @@ namespace Assets.Scripts.View
     {
         private const int boardSize = 8;
 
-        [SerializeField] GameObject cellProto; //prototype of board cell
-        [SerializeField] GameObject allowedCellProto; //prototype of board cell collider(collider for allowed cells)
-        [SerializeField] GameObject blackChip; //prototype of black chip
-        [SerializeField] GameObject whiteChip; // prototype of white chip
-        [SerializeField] GameObject cells;  //parent of all cells that would be generated
-        [SerializeField] GameObject chips;  // parent of all chips that would be added
-        [SerializeField] GameObject cellColliders; //parent of all board cell colliders (colliders for allowed cells)
+        [SerializeField]
+        GameObject gameBoard; //game board object on the scene
+
+        [SerializeField] 
+        GameObject cellProto; //prototype of board cell
+
+        [SerializeField]
+        HumanController controller; //human controller
+
+        [SerializeField] 
+        Button allowedCellProto; //prototype of board cell collider(collider for allowed cells)
+
+        [SerializeField] 
+        GameObject blackChip; //prototype of black chip
+
+        [SerializeField] 
+        GameObject whiteChip; // prototype of white chip
+
+        [SerializeField] 
+        GameObject cells;  //parent of all cells that would be generated
+
+        [SerializeField] 
+        GameObject chips;  // parent of all chips that would be added
+
+        [SerializeField] 
+        GameObject cellColliders; //parent of all board cell colliders (colliders for allowed cells)
 
         private GameObject[,] boardCells;
         private GameObject[,] existedChips;
-        private GameObject[,] allowedCells;
+        private Button[,] allowedCells;
 
         public void ClearAll()
         {
@@ -59,16 +78,16 @@ namespace Assets.Scripts.View
         {
             foreach (Cell allowedCell in allowedCells)
             {
-                this.allowedCells[allowedCell.X, allowedCell.Y].SetActive(true);
+                this.allowedCells[allowedCell.X, allowedCell.Y].gameObject.SetActive(true);
             }
         }
 
         public void ClearAllowedCells()
         {
             //disable to press on all colliders
-            foreach (GameObject cell in allowedCells)
+            foreach (Button cell in allowedCells)
             {
-                cell.SetActive(false);
+                cell.gameObject.SetActive(false);
             }
         }
 
@@ -84,29 +103,40 @@ namespace Assets.Scripts.View
 
         public void GenerateBoard()
         {
-            float startX = -473, startY = 530, step = 135;
+            Vector2 boardPosition = gameBoard.transform.position;
+            Vector2 cellSize = cellProto.transform.localScale;
+
+            float startX = boardPosition.x - cellSize.x * (boardSize - 1) / 2;
+            float startY = boardPosition.y + cellSize.y * (boardSize - 1) / 2;
+
+            float step = cellSize.x;
+
+            float currentX = startX;
+            float currentY = startY;
+
             for (int i = 0; i < boardSize; i++)
             {
                 for (int j = 0; j < boardSize; j++)
                 {
                     // generate board cells
-                    boardCells[i, j] = Instantiate(cellProto, new Vector2(startX, startY), cellProto.transform.rotation);
+                    boardCells[i, j] = Instantiate(cellProto, new Vector2(currentX, currentY), cellProto.transform.rotation);
                     boardCells[i, j].name = i + "" + j;
                     boardCells[i, j].transform.SetParent(cells.transform);
 
                     //generate colliders
-                    allowedCells[i, j] = Instantiate(allowedCellProto, new Vector2(startX, startY), cellProto.transform.rotation);
+                    allowedCells[i, j] = Instantiate<Button>(allowedCellProto, new Vector2(currentX, currentY), cellProto.transform.rotation);
                     allowedCells[i, j].name = i + "" + j;
                     allowedCells[i, j].transform.SetParent(cellColliders.transform);
-                    allowedCells[i, j].SetActive(false);
+                    allowedCells[i, j].gameObject.SetActive(false);
+                    allowedCells[i, j].onClick.AddListener(controller.OnClicked);
 
-                    startX += step;
+                    currentX += step;
 
                     if (j == boardSize - 1)
-                        startX = -473;
+                        currentX = startX;
                 }
 
-                startY -= step;
+                currentY -= step;
             }
         }
 
@@ -114,7 +144,7 @@ namespace Assets.Scripts.View
         {
             boardCells = new GameObject[boardSize, boardSize];
             existedChips = new GameObject[boardSize, boardSize];
-            allowedCells = new GameObject[boardSize, boardSize];
+            allowedCells = new Button[boardSize, boardSize];
 
             GenerateBoard();
         }
